72bc4cd8b6e1fe83e124008bae34001dc0bade5d
ruchi bhindwale <rbhindwale@ruchis-MacBook-Pro.local>
2019-05-16 16:53:24 +0530

    Fine tuning navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,34 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const Layout = props => (
-    <Fragment>
-        <div>Navigation</div>
-        <main className={classes.Content}>{props.children}</main>
-    </Fragment>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer : false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    }
+
+    toggleSideDrawerHandler = (toggleVal) => {
+        this.setState((prevState) => {
+            return {showSideDrawer : toggleVal ? toggleVal : !prevState.showSideDrawer}
+        })
+    }
+
+    render () {
+        return (
+        <Fragment>
+            <Toolbar toggleSideDrawer={this.toggleSideDrawerHandler}/>
+            <SideDrawer click={this.sideDrawerClosedHandler} open={this.state.showSideDrawer} closed={() => this.sideDrawerClosedHandler(false)}/>
+            <main className={classes.Content}>{this.props.children}</main>
+        </Fragment>
+        )
+    }
+}
 
 export default Layout;
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,29 @@
+import React, {Fragment} from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+
+import classes from './SideDrawer.css'
+
+const sideDrawer = props => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
+    return(
+        <Fragment>
+            <Backdrop show={props.open} click={props.closed}/>
+            <div onClick={props.click} className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo/>
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Fragment>
+    );
+}
+
+export default sideDrawer;
